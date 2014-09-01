6c48097d9726e1b577c19cd1ae2792cc8fa346ab
Kai INUI <lied.der.optik@gmail.com>
2014-09-02 04:13:03 +0900

    init

---
+++ SugarToast/KIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KIAppDelegate.h
+//  SugarToast
+//
+//  Created by kaiinui on 2014/09/02.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SugarToast/KIViewController.h
@@ -0,0 +1,13 @@
+//
+//  KIViewController.h
+//  SugarToast
+//
+//  Created by kaiinui on 2014/09/02.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIViewController : UIViewController
+
+@end
